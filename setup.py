95545388076ec8721137f1f4074596d96d9012e8
Joe Aguirre <joseph.aguirre@lfg.com>
2014-01-14 20:24:49 -0600

    remove version from setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup
-from duo_log_grabber import __version__
 
 setup(
     name='duo_log_grabber',
-    version=__version__,
     description='Retrieves logs from Duo Security API and sends CEF-compliant syslog messages.',
     author='Joe Aguirre',
     url='https://github.com/libresec/duo_log_grabber/',
     license='GPLv3+',
-    install_requires=['duo_client', 'loggerglue'],
-    dependency_links=['https://github.com/duosecurity/duo_client_python/tarball/master#egg=duo_client_python'],
+    install_requires=['duo_client_python', 'loggerglue'],
+    download_url=['https://github.com/duosecurity/duo_client_python/tarball/master#egg=duo_client_python'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
